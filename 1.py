083bf13be2c6359ad47a8f3ef876c42b5b567b8b
ihsansaktia <ihsan.saktia@gmail.com>
2018-02-24 11:03:09 +0700

    Fixed plotting

--- 1.py
+++ 1.py
@@ -1,14 +1,12 @@
 # =============================
 # Libraries
 # =============================
-import numpy as np
-import matplotlib.pyplot as plt
-from math import hypot as hyp
-from copy import deepcopy as dcopy
-import random
 import os
-import functools 
-import math
+import random
+import numpy as np
+from math import atan2
+from math import hypot as hyp
+import matplotlib.pyplot as plt
 # =============================
 
 # If set of points P are used to draw a convex polygon G 
@@ -22,12 +20,12 @@ def getCentre(P):
 	centre = [x/lp, y/lp]
 	return centre
 
-# For a line L that passes through a point A to point B
+# For a line L that passes through point A to point B
 # This function determines on which side of L is P located 
 def whichSide(A, B, P):
 	return (P[0]-A[0])*(B[1]-A[1]) - (P[1]-A[1])*(B[0]-A[0])
 
-# For a line L that passes through a point A to point B
+# For a line L that passes through point A to point B
 # This function returns the perpendicular distance from P to L
 def distPTL(A, B, P):
 	print(A, B, P)
@@ -36,69 +34,80 @@ def distPTL(A, B, P):
 	print(numerator, denominator)
 	return numerator/denominator
 
+# This function initializes recursive calls for finding hulls
 def solveQH(P, CH):
-
-	# This variable contains points of Hull
-	# CH = []
-
-	# Sorting set of points
+	# Sorting set of points by X first, then by Y
 	P.sort(key = lambda x : (x[0], x[1]))
 
 	# Finding leftmost point, rightmost point
-	# Lets say L is line drawn from leftmost point to rightmost point
+	# Lets say L is a line drawn from leftmost point to rightmost point
 	# A random point located at the top of the line L is needed to determine on which side of the line L that a point is located
 	leftmost = P[0]
 	rightmost = P[-1]
 	aTopPoint = [leftmost[0], leftmost[1]+1]
-	dTop = whichSide(leftmost, rightmost, aTopPoint)
+	topSide = whichSide(leftmost, rightmost, aTopPoint)
 
+	# Set of points located in the top side and bottom side of line L respectively
 	topP = []
 	bottomP = []
-	for point in P[1:-1]:
-		print("Ini point yang mau dicari", point)
-		t = whichSide(leftmost, rightmost, point)
-		# b = whichSide(rightmost, leftmost, point)
-		if t != 0 and np.sign(dTop) == np.sign(t):
-			topP.append(point)
-		else:
-			bottomP.append(point)
 
-	# print("Ini P, topP, botP", P, topP, bottomP)
-	print("Ini P", P)
-	print("Ini topP", topP)
-	print("Ini bottomP", bottomP)
+	# Dividing points 
+	for point in P:
+		if point != leftmost and point != rightmost:
+			# Get the side of this particular point
+			currSide = whichSide(leftmost, rightmost, point)
+
+			# If it is not in the line L, and it is on the top of L
+			if currSide != 0 and np.sign(topSide) == np.sign(currSide):
+				topP.append(point)
+			else:
+				bottomP.append(point)
+
 	topH = FindHull(topP, leftmost, rightmost)
 	bottomH = FindHull(bottomP, rightmost, leftmost)
-	print("Ini topH dan botH", topH, bottomH)
 	CH += [leftmost] + topH + bottomH + [rightmost]
 
+# For a line X that passes through leftmost point to rightmost point
+# This function searches for the farthest point P
+# then recursively divide point thats located on the top side of line that passes through leftmost point to P 
+# and recursively divide point thats located on the top side of line that passes through P to rightmost point 
 def FindHull(P, leftmost, rightmost):
+	# If theres no more point
 	if len(P) == 0:
+		# return empty set of point
 		return []
 	else:
-		currMinDist = -1
+		# Initialize distance to -1 as distance cant be negative 
+		currMaxDist = -1
+
+		for point in P:
+			# Calculate distance of particular point to the line
+			pointDist = distPTL(leftmost, rightmost, point)
+			currMaxDist = max(currMaxDist, pointDist)
+			if currMaxDist == pointDist:
+				currP = point
+
+		listofHull = [currP]
+
 		for point in P:
 			pointDist = distPTL(leftmost, rightmost, point)
-			currMinDist = max(currMinDist, pointDist)
-			if currMinDist == pointDist:
-				currP = dcopy(point)
+			if currMaxDist == pointDist:
+				listofHull.append(point)
+
 		topP = []
 		botP = []
-		ltop = []
-		ltop = rightmost
-		rtop = leftmost
-		# rtop = [rightmost[0]+1, rightmost[1]+1]
-		dlTop = whichSide(leftmost, currP, ltop)
-		drTop = whichSide(currP, rightmost, rtop)
+		sideRightmost = whichSide(leftmost, currP, rightmost)
+		sideLeftmost = whichSide(currP, rightmost, leftmost)
 		for point in P:
-			if point == currP:
-				continue
-			else:
+			# if point == currP:
+			# 	continue
+			# else:
+			if point not in listofHull:
 				l = whichSide(leftmost, currP, point)
 				r = whichSide(currP, rightmost, point)
-				if l != 0 and np.sign(l) != np.sign(dlTop):
+				if np.sign(l) != np.sign(sideRightmost):
 					topP.append(point)
-				if r != 0 and np.sign(r) != np.sign(drTop):
+				if np.sign(r) != np.sign(sideLeftmost):
 					botP.append(point)
 
 		print("Ini LRMost dan currP", leftmost, currP, rightmost)
@@ -107,25 +116,19 @@ def FindHull(P, leftmost, rightmost):
 		tH = FindHull(topP, leftmost, currP)
 		bH = FindHull(botP, currP, rightmost)
 		print("Ini currP", currP, tH, bH)
-		return [currP] + tH + bH
+		return listofHull + tH + bH
 
 if __name__ == "__main__":
 	os.system("cls")
 	nPoint = int(input("Number of points: "))
 	maxP = int(input("Maximum value of point: "))
-	# genX = [item for item in np.random.randint(0, maxP, nPoint)]
-	# genY = [item for item in np.random.randint(0, maxP, nPoint)]
-	genX = [x for x in [random.randint(0, maxP) for _ in range(nPoint)]]
-	genY = [x for x in [random.randint(0, maxP) for _ in range(nPoint)]]
-	# print(genX)
-	# print(genY)
-	# genY = rand(0, maxP, nPoint)
+	genX = [x for x in [np.random.random_integers(0, maxP) for _ in range(nPoint)]]
+	genY = [x for x in [np.random.randint(0, maxP) for _ in range(nPoint)]]
 	P = [[item[0], item[1]] for item in zip(genX, genY)] 
-	# print(P)
 	ans = []
 	solveQH(P, ans)
 	centre = getCentre(ans)
-	ans.sort(key = lambda x : (math.atan2(x[1]-centre[1], x[0]-centre[0])))
+	ans.sort(key = lambda x : (atan2(x[1]-centre[1], x[0]-centre[0])))
 	# ans.append(ans[0])
 	print(ans)
 	ansX, ansY = [x for x in zip(*ans)]
@@ -133,10 +136,16 @@ if __name__ == "__main__":
 	ansY = list(ansY)
 	ansX.append(ans[0][0])
 	ansY.append(ans[0][1])
-	print(list(ansX), list(ansY))
-	plt.plot(genX, genY, "bo")
-	plt.plot(list(ansX), list(ansY), "ro")
-	plt.plot(list(ansX), list(ansY), "r-")
+	# print(list(ansX), list(ansY))
+	plt.plot(genX, genY, "go")
+	plt.plot(list(ansX), list(ansY), "ro-")
+	# plt.plot(list(ansX), list(ansY), "r-")
 	# plt.plot([1,2,3], [4,5,6], "ro")
+	# ans = set(ans)
+	for i in range(len(ans)-1):
+		l = ans[i]
+		r = ans[i+1]
+		if l != r:
+			print(ans[i], ans[i+1])
 	plt.show()
 
